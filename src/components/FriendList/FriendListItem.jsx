fbdeab666b3f27b29ed2c9050ca01f9a5272a59b
Olga16119 <tkachenko93@ukr.net>
2023-07-10 12:17:33 +0200

    update homework_react_01

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,18 +3,15 @@ import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 
 export const FriendListItem = ({ name, avatar, isOnline }) => {
-    const status = isOnline ? `online` : `ofline`;
     return(
- <li className={css.item}>
-            <span className={css.status}>{ status}</span>
+          <li className={css.item}>
+            <span className={isOnline?css.statusOnline: css.statusOfline}></span>
   <img className={css.avatar} src={avatar} alt={name} width="48" />
             <p className={css.name}>{name }</p>
 </li>)
 }
 
-
 FriendListItem.protoTypes = {
-      id: PropTypes.number.isRequired,
       name: PropTypes.string.isRequired,
       avatar: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
